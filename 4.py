b7bf7a933fd2d54d3375af7b9fd7e746e7dc4a63
Max Ohsawa <max.ohsawa@gmail.com>
2024-12-11 23:29:33 -0500

    adds day 4, part 2

--- 4.py
+++ 4.py
@@ -1,4 +1,6 @@
 grid = None
+height = None
+width = None
 
 def parse_file(filename):
   with open(filename, "r") as file:
@@ -16,8 +18,6 @@ DIRECTIONS = [
 ]
 
 def check_direction(grid, y, x, dy, dx):
-  height = len(grid)
-  width = len(grid[0])
 
   if not (0 <= y + 3*dy < height and 0 <= x + 3*dx < width):
     return 0
@@ -30,11 +30,7 @@ def check_direction(grid, y, x, dy, dx):
   return 0
   
 def count_xmasses(grid):
-
-  height = len(grid)
-  width = len(grid[0])
   count = 0
-
 
   for y in range(height):
     for x in range(width):
@@ -44,13 +40,41 @@ def count_xmasses(grid):
 
   return count
 
+def is_m_s_pair(a, b):
+  return (a == 'M' and b =='S') or (a == 'S' and b == 'M')
+
+def count_x_masses(grid):
+  count = 0
+
+  for y in range(1, height - 1):
+    for x in range(1, width - 1):
+      if grid[y][x] == 'A':
+        
+        # NW to SE diagonal
+        nw_char = grid[y + -1][x + -1]
+        se_char = grid[y + 1][x + 1]
+        # NE to SW diagonal
+        ne_char = grid[y - 1][x + 1]
+        sw_char = grid[y + 1][x - 1]
+        if is_m_s_pair(nw_char, se_char) and \
+           is_m_s_pair(ne_char, sw_char):
+          count += 1
+          
+  return count
+
 if __name__ == "__main__":
   import sys
 
   filename = sys.argv[1] if len(sys.argv) > 1 else "4.txt"
 
   grid = parse_file(filename)
+  height = len(grid)
+  width = len(grid[0])
 
   sum_of_xmasses = count_xmasses(grid)
 
   print("part_1: ", sum_of_xmasses)
+
+  sum_of_x_masses = count_x_masses(grid)
+
+  print("part 2: ", sum_of_x_masses)
